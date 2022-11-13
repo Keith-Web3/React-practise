8d00dca250d1d98c28ef04a1d3e6c3588ea97cd6
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2022-11-13 16:16:44 +0100

    add

--- business-card/src/components/About.jsx
+++ business-card/src/components/About.jsx
@@ -7,7 +7,7 @@ export default function () {
       <p>
         I am a frontend developer with a particular interest in making things
         simple and automating daily tasks. I try to keep up with security and
-        best practices, and am always looking for new things to learn.
+        best practices, and am always looking for new thingss to learn.
       </p>
     </section>
   )
